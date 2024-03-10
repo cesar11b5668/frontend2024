9fd4cbfdf9c6c3cba47ba04c3441f6f7d5904215
cesar desgarennes sanchez <cesardessanz@gmail.com>
2024-03-09 22:38:16 -0600

    calculator

---
+++ inserta_datos/src/App.jsx
@@ -0,0 +1,32 @@
+const persona = (
+   nombre: "Cesar Desgarennes Sanchez",
+   edad: 22,
+   ocupacion: "estudiante",
+   pasatiempos: [
+    "escuchar musica",
+    "ver anime",
+    "jugar videojuegos"
+   ]
+
+  )
+
+function App() {
+  return (
+    <div><h1>informacion personal</h1>
+    <hr></hr>
+    <ul>
+      <li>nombre: {persona.nombre}</li>    
+      <li>edad: {persona.edad}</li>
+      <li>numero: {persona.numero}</li>    
+    </ul>
+    </div>
+  )
+}
+
+export default App
+
+// 2 hobits a la lista
+// signo zodiacal
+// fecha de nacimiento
+// lugar de nacimiento
+
